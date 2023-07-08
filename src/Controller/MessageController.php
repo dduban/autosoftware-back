ec3cb6257f36cd0b8ce74ac6891444154dea84af
dduban <dominika.dubaniowska@gmail.com>
2023-07-08 14:38:08 +0200

    save message in messageController

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Message;
+use App\Repository\MessageRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[Route('/message', name: 'message_', methods: ['OPTIONS', 'POST', 'GET'])]
 class MessageController extends AbstractController
 {
-    #[Route('/message', name: 'app_message')]
-    public function index(): JsonResponse
+    private MessageRepository $messageRepository;
+    private SerializerInterface $serializer;
+
+    public function __construct(
+        MessageRepository $messageRepository,
+    )
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MessageController.php',
-        ]);
+        $this->messageRepository = $messageRepository;
     }
+
+
+    #[Route('/save', name: 'save', methods: ['POST'])]
+    public function saveMessage(Request $request): JsonResponse
+    {
+        $message = $this->serializer->deserialize($request->getContent(), Message::class, 'json');
+
+        $message->setCreateDate(new \DateTime());
+
+        $this->messageRepository->save($message, true);
+
+        return new JsonResponse(['uuid' => $message->getId()], 201);
+    }
+
+
 }
